1c86add66c3561794f65dd8015747cae4cb84955
Skarsky <anishs1882001@gmail.com>
2020-06-14 10:47:08 -0400

    flask routes boilerplate 0

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
+def root():
+    return "Welcome to Stockastic!"
+
+
+@app.route("/TICKER/models")
 def index():
-  return "Welcome to Stockastic!"
+    return "Requested prediction models"
+
+
+@app.route("/TICKER/model/MODEL_NAME")
+def show():
+    return "Descriptive look at model"
+
+
+@app.route("/TICKER/model/MODEL_NAME/train")
+def train():
+    return "Train the MODEL_NAME model by tuning the following hyperparameters"
+
+
+@app.route("/TICKER/model/MODEL_NAME/predict")
+def predict():
+    return "Predict price of TICKER at a custom date"
+
 
 if __name__ == "__main__":
-  app.run(debug = True)
+    app.run(debug=True)

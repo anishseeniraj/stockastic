a249e2d4564c6403319cf3df3531d28c82f9a401
Skarsky <anishs1882001@gmail.com>
2020-07-11 10:55:52 -0400

    implemented selective models display

--- app.py
+++ app.py
@@ -46,29 +46,41 @@ def ticker():
     df = read_historic_data(ticker)  # read past 5y stock performance data
     historic_plot = historic_model(df)  # plot historic price
     plots = []
+    show_plot = {  # bool check to selectively show plots in the template
+        "ma": 0,
+        "lr": 0,
+        "knn": 0,
+        "lstm": 0,
+        "arima": 0
+    }
 
     for model in ["ma", "lr", "knn", "lstm", "arima"]:
         value = request.form.get(model)
 
         if value:
             if value == "ma":
+                show_plot["ma"] = 1
                 moving_average_plot, ma_rmse = moving_average_model(df)
 
                 plots.append(moving_average_plot)
             elif value == "lr":
+                show_plot["lr"] = 1
                 linear_model, linear_fig, linear_regression_plot, lr_rmse = linear_regression_model(
                     df)
 
                 plots.append(linear_regression_plot)
             elif value == "knn":
+                show_plot["knn"] = 1
                 k_model, knn_fig, knn_plot, knn_rmse = knn_model(df)
 
                 plots.append(knn_plot)
             elif value == "lstm":
+                show_plot["lstm"] = 1
                 lstm, lstm_fig, lstm_plot, lstm_rmse = lstm_model(df)
 
                 plots.append(lstm_plot)
             elif value == "arima":
+                show_plot["arima"] = 1
                 auto_arima_plot, arima_rmse = auto_arima_model(df)
 
                 plots.append(auto_arima_plot)
@@ -79,6 +91,7 @@ def ticker():
         "models.html.jinja",
         ticker=ticker,
         historic_plot=historic_plot,
+        show_plot=show_plot,
         moving_average_plot=plots[0],
         linear_regression_plot=plots[1],
         knn_plot=plots[2],
